e37d4138a044a596ecb5be3503f969872546c1dc
Richard Chan <richardchankiyin@gmail.com>
2022-04-03 20:29:50 +0800

    add logging messages

--- SimpleAuthenticationService/bm-hk/src/test/java/com/hsbc/hk/AppTest.java
+++ SimpleAuthenticationService/bm-hk/src/test/java/com/hsbc/hk/AppTest.java
@@ -7,6 +7,8 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.util.Optional;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.junit.Test;
 
@@ -15,18 +17,30 @@ import org.junit.Test;
  */
 public class AppTest 
 {
+	private static final Logger logger;
+	static {
+		String path = TokenImplTest.class.getClassLoader()
+                .getResource("loggingtest.properties")
+                .getFile();
+		System.setProperty("java.util.logging.config.file", path);
+		logger = Logger.getLogger(AppTest.class.getName());
+	}
+	
 
     @Test
     public void testUserAndRoleCreation() throws Exception
     {
+    	logger.info("Running testUserAndRoleCreation...");
         AccessPoint ap = new AccessPoint();
         AuthenticationService as = ap.createInstance();
         String user1 = "user1";
         String password1 = "password1";
         
         User userObj1 = as.createUser(user1, password1);
+        logger.log(Level.INFO, "user {0} created", userObj1);
         String role1 = "role1";
         Role roleObj1 = as.createRole(role1);
+        logger.log(Level.INFO, "role {0} created", roleObj1);
         as.addRoleToUser(roleObj1, userObj1);
         
         assertNotNull(userObj1);
@@ -35,17 +49,21 @@ public class AppTest
     
     @Test
     public void testAuthenticateAndRoleChecking() throws Exception {
+    	logger.info("Running testAuthenticateAndRoleChecking...");
     	AccessPoint ap = new AccessPoint();
         AuthenticationService as = ap.createInstance();
         String user1 = "user1";
         String password1 = "password1";
         
         User userObj1 = as.createUser(user1, password1);
+        logger.log(Level.INFO, "user {0} created", userObj1);
         String role1 = "role1";
         Role roleObj1 = as.createRole(role1);
+        logger.log(Level.INFO, "role {0} created", roleObj1);
         as.addRoleToUser(roleObj1, userObj1);
         
         Token token1 = as.authenticate(user1, password1, "");
+        logger.log(Level.INFO, "user {0} login with token: {1}", new Object[] {userObj1, token1});
         assertTrue(as.checkRole(token1, roleObj1));
         assertTrue(as.getAllRoles(token1).contains(roleObj1));
         assertEquals(Optional.of(roleObj1), as.findRole(role1));
@@ -63,17 +81,21 @@ public class AppTest
     
     @Test
     public void testMaintainUserAndRole() throws Exception{
+    	logger.info("Running testMaintainUserAndRole...");
     	AccessPoint ap = new AccessPoint();
         AuthenticationService as = ap.createInstance();
         String user1 = "user1";
         String password1 = "password1";
         
         User userObj1 = as.createUser(user1, password1);
+        logger.log(Level.INFO, "user {0} created", userObj1);
         String role1 = "role1";
         Role roleObj1 = as.createRole(role1);
+        logger.log(Level.INFO, "role {0} created", roleObj1);
         as.addRoleToUser(roleObj1, userObj1);
         
         Token token1 = as.authenticate(user1, password1, "");
+        logger.log(Level.INFO, "role {0} created", roleObj1);
         assertTrue(as.checkRole(token1, roleObj1));
         
         as.deleteRole(roleObj1);
