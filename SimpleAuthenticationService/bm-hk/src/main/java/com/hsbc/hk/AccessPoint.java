b757e9f7e94c6f17fb0c276a56e9d523734b8005
Richard Chan <richardchankiyin@gmail.com>
2022-04-03 20:16:53 +0800

    implement AccessPoint and AppTest

--- SimpleAuthenticationService/bm-hk/src/main/java/com/hsbc/hk/AccessPoint.java
+++ SimpleAuthenticationService/bm-hk/src/main/java/com/hsbc/hk/AccessPoint.java
@@ -1,20 +1,16 @@
 package com.hsbc.hk;
 
- 
-
- 
-
 import jdk.jshell.spi.ExecutionControl;
-
- 
 
 public class AccessPoint {
 
     public AuthenticationService createInstance() throws ExecutionControl.NotImplementedException {
-
-        // @TODO implement this method to return a concrete instance of @see AuthenticationService interface implementation
-
-        throw new ExecutionControl.NotImplementedException("Not implemented");
+        // implement this method to return a concrete instance of @see AuthenticationService interface implementation
+    	UserManager userManager = new UserManager();
+    	RoleManager roleManager = new RoleManager();
+    	TokenManager tokenManager = new TokenManager(30 * 60);
+    	AuthenticationManager authenticationManager = new AuthenticationManager(userManager, tokenManager);
+    	return new AuthenticationServiceImpl(userManager, roleManager, tokenManager, authenticationManager);
 
     }
 
--- SimpleAuthenticationService/bm-hk/src/test/java/com/hsbc/hk/AppTest.java
+++ SimpleAuthenticationService/bm-hk/src/test/java/com/hsbc/hk/AppTest.java
@@ -1,6 +1,12 @@
 package com.hsbc.hk;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.Optional;
 
 import org.junit.Test;
 
@@ -9,12 +15,69 @@ import org.junit.Test;
  */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
+
     @Test
-    public void shouldAnswerWithTrue()
+    public void testUserAndRoleCreation() throws Exception
     {
-        assertTrue( true );
+        AccessPoint ap = new AccessPoint();
+        AuthenticationService as = ap.createInstance();
+        String user1 = "user1";
+        String password1 = "password1";
+        
+        User userObj1 = as.createUser(user1, password1);
+        String role1 = "role1";
+        Role roleObj1 = as.createRole(role1);
+        as.addRoleToUser(roleObj1, userObj1);
+        
+        assertNotNull(userObj1);
+        assertNotNull(roleObj1);
+    }
+    
+    @Test
+    public void testAuthenticateAndRoleChecking() throws Exception {
+    	AccessPoint ap = new AccessPoint();
+        AuthenticationService as = ap.createInstance();
+        String user1 = "user1";
+        String password1 = "password1";
+        
+        User userObj1 = as.createUser(user1, password1);
+        String role1 = "role1";
+        Role roleObj1 = as.createRole(role1);
+        as.addRoleToUser(roleObj1, userObj1);
+        
+        Token token1 = as.authenticate(user1, password1, "");
+        assertTrue(as.checkRole(token1, roleObj1));
+        assertTrue(as.getAllRoles(token1).contains(roleObj1));
+        assertEquals(Optional.of(roleObj1), as.findRole(role1));
+        assertEquals(Optional.of(userObj1), as.findUser(user1));
+        
+        as.invalidate(token1);
+        
+        try {
+        	as.checkRole(token1, roleObj1);
+        	fail("should not reach here");
+        } catch (RuntimeException re) {
+        	assertEquals("token is invalid", re.getMessage());
+        }
+    }
+    
+    @Test
+    public void testMaintainUserAndRole() throws Exception{
+    	AccessPoint ap = new AccessPoint();
+        AuthenticationService as = ap.createInstance();
+        String user1 = "user1";
+        String password1 = "password1";
+        
+        User userObj1 = as.createUser(user1, password1);
+        String role1 = "role1";
+        Role roleObj1 = as.createRole(role1);
+        as.addRoleToUser(roleObj1, userObj1);
+        
+        Token token1 = as.authenticate(user1, password1, "");
+        assertTrue(as.checkRole(token1, roleObj1));
+        
+        as.deleteRole(roleObj1);
+        assertTrue(as.findRole(role1).isEmpty());
+        assertFalse(as.checkRole(token1, roleObj1));
     }
 }
